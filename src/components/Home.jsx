a32a492c54446b2f2ae011a3b9a15eaf3f5eda20
Joseph Diaz <jgdazuaje20@gmail.com>
2024-03-09 21:09:26 -0400

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,86 @@
 
 import './Home.css'
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Home = () => {
   return (
     <div className="Information">
-          <Navbar expand="lg" className="bg-body-tertiary Nav " >
+      
+    <div className='Carrusel'>
+
+ <h1>Joseph Diaz</h1>
+ <h2>Front-end Developer</h2>
+ <div className='Btns'>
+ <button onClick={() => window.location.href = '#About-me'}>
+  About Me
+ </button>
+ <button onClick={() => window.location.href = '#Education'}>
+  Skills
+ </button>
+ <button onClick={() => window.location.href = '#Projects'}>
+  Projects
+ </button>
+ </div>
+ </div>
+      <div id="About-me" className="About-me">
+        <h1>About me</h1>
+        <div className='About-me-text'>
+        <p>I am responsible, organized  individual who focuses on achieving the set objectives in the most optimal way possible, adaptable to the situation, and constantly seeks improvement.</p>
+         <p>I started coding since march 2023 and from that moment to the present i discover an loved the  front-end development world learning both self taugh and with academies </p>
+        </div>
+       
+        <h1 id="Education"> Skills</h1>
+        
+       <div className='Studies'>
+       
+        <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_html_icon_130541.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_css_icon_130661.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_git_icon_130581.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2406/PNG/512/github_git_icon_145985.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/bootstrap_plain_logo_icon_146619.png' className='img-icons'/>
+        <img src='https://cdn.icon-icons.com/icons2/2699/PNG/512/firebase_logo_icon_171157.png' className='img-icons'/>
+       </div>
+        
+        <h2>Education</h2>
+      </div>
+     
+      <div  className="Education">
+       <div className='Card-Certificates'>  <img src="https://media.licdn.com/dms/image/D4E2DAQGWwuTCB2781A/profile-treasury-image-shrink_800_800/0/1705529594218?e=1710205200&v=beta&t=UZS0yCvUhVoJfHutymfgqVvgO0ulEyPtw0Z1O-Jh7m4" alt="Javascript"/></div>
+
+       <div className='Card-Certificates'><img src="https://media.licdn.com/dms/image/D4E2DAQHlQzhjYLpDeA/profile-treasury-image-shrink_800_800/0/1708274576082?e=1710205200&v=beta&t=wULFsTvAOb5tgntQ67pR-vkW56BrL-RbMJ8WFr-3bv8" alt="React JS"/></div>
+      </div>
+      
+      <div id="Projects" className="Projects">
+  <h1>Projects</h1>
+  <card className="Card">
+    <span className="title">Logic</span>
+    <div>
+    <a href="https://65d3dbc49e9f6b454e5000e7--spiffy-cranachan-c4f1fd.netlify.app"><img src="https://i.stack.imgur.com/S4law.jpg?s=256&g=1"></img></a>
+    </div>
+  
+  </card>
+  </div>
+
+     <div className="Footer">
+   <a href='https://www.linkedin.com/in/joseph-díaz-2a866729b' className='Link'><FontAwesomeIcon icon={faLinkedin} /></a>
+   <a href='https://github.com/JosephD002' className='Link'><FontAwesomeIcon icon={faGithub} /></a>
+   
+     </div>
+        </div>
+        
+  )
+}
+
+export default Home
+
+
+/** <Navbar expand="lg" className="bg-body-tertiary Nav " >
       <Container>
         <Navbar.Brand href="#home" className='text'>Joseph Diaz</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -24,56 +94,32 @@ const Home = () => {
       </Container>
     </Navbar>
 
-    <div className='Carrusel'>
 
-<h1>Joseph Diaz</h1>
-<h2>Front-end Developer</h2>
-<div className='Btns'>
-<button onClick={() => window.location.href = '#About-me'}>
-About Me
-</button>
-<button onClick={() => window.location.href = '#Education'}>
-Education
-</button>
-<button onClick={() => window.location.href = '#Projects'}>
-Projects
-</button>
-</div>
-</div>
-      <div id="About-me" className="About-me">
-        <h1>About me</h1>
-        <p>Im a person who start to love coding since 2022,always trying to know more about this beatiful world and always trying to be the best version i can be!</p>
-
-        <h1> Education</h1>
-        <p>I currently have knowledge in HTML | CSS | Javascript | React JS | Git | Node.js and database management in Firebase. </p>
-        <h2>Certificates</h2>
-      </div>
-     
-      <div id="Education" className="Education">
-       <div className='Card-Certificates'>  <img src="https://media.licdn.com/dms/image/D4E2DAQGWwuTCB2781A/profile-treasury-image-shrink_800_800/0/1705529594218?e=1709244000&v=beta&t=3U-RTWjmijfzy5SPg5X0ekhMWgGX-sPPGUZAddLCI8M" alt="Javascript"/></div>
-
-       <div className='Card-Certificates'><img src="https://media.licdn.com/dms/image/D4E2DAQHlQzhjYLpDeA/profile-treasury-image-shrink_800_800/0/1708274576082?e=1709244000&v=beta&t=r7-HKKfsSSk1IRJQAHfBoZUMtrQKMzdtK_wV4uNwJq4" alt="React JS"/></div>
-      </div>
-      
-      <div id="Projects" className="Projects">
-  <h1>Projects</h1>
-  <card className="Card">
-    <span className="title">Project React</span>
-    <div>
-    <a href="https://65d3dbc49e9f6b454e5000e7--spiffy-cranachan-c4f1fd.netlify.app"><img src="https://i.stack.imgur.com/S4law.jpg?s=256&g=1"></img></a>
-    </div>
-  
-  </card>
-</div>
-
-     <div className="Footer">
-   <p>  <FontAwesomeIcon icon={faEnvelope} />:jgdiaz002@gmail.com</p>
-     </div>
-        </div>
-        
-  )
-}
-
-export default Home
-
-
+      <ul>
+           <li>
+          <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_html_icon_130541.png' className='img-icons'></img>
+           </li>
+           <li>
+           <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_css_icon_130661.png' className='img-icons'></img>
+           </li>
+           <li>
+           <img src='https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png' className='img-icons'></img>
+            </li>
+            <li>
+            <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png' className='img-icons'></img>
+            </li>
+            <li>
+            <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_git_icon_130581.png' className='img-icons'></img>
+            </li>
+            <li>
+            <img src='https://cdn.icon-icons.com/icons2/2406/PNG/512/github_git_icon_145985.png' className='img-icons'></img>
+            </li>
+            <li>
+            <img src='https://cdn.icon-icons.com/icons2/2415/PNG/512/bootstrap_plain_logo_icon_146619.png' className='img-icons'></img>
+            </li>
+            <li>
+            <img src='https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_node_icon_130301.png' className='img-icons'></img>
+            </li>
+           <li><p>Next.JS(learning)</p></li>
+        </ul>
+ */
